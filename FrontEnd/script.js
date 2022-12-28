a55715f13f07110451cf05518a0455f828ef4b75
Louisa Ongenyi <o.louisa@ymail.com>
2022-12-28 01:48:43 +0100

    Récupération des données get

---
+++ FrontEnd/script.js
@@ -0,0 +1,60 @@
+let user = {
+    email: "sophie.bluel@test.tld",
+    password: "S0phie"
+};
+
+fetch("http://localhost:5678/api/users/login", {
+    method: "post",
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+
+})
+.then(res => res.json())
+.then(res => console.log(res))
+
+
+fetch("http://localhost:5678/api/categories", {
+    headers: {
+        'Accept': 'application/json',
+    }
+})
+    .then(response => response.json()
+    )
+    .then(response2 =>
+        console.log(response2)
+    )
+// .then((response)=>{
+// console.log(response.json());
+// })
+// .then((response2) =>{
+//     console.log(response2);
+// })
+
+
+fetch("http://localhost:5678/api/works", {
+    headers: {
+        'Accept': 'application/json'
+    }
+})
+    .then(response => response.json())
+    .then(response2 => console.log(response2))
+
+// Route Works avec la méthode POST:
+fetch("http://localhost:5678/api/works", {
+    method: "post",
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY3MjE4NTg4MCwiZXhwIjoxNjcyMjcyMjgwfQ.XVCc74rr73yBxGunE2AHTssTHdWQIPNPmvXYju6QdSI',
+      'Content-Type': 'multipart/form-data' 
+    }
+    // body: JSON.stringify({
+
+    // })
+})
+    .then(response => response.json())
+    .then(response2 => console.log(response2));
+
+console.log("test");
