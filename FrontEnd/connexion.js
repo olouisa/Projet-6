1674de3c089d31c1cf705f4d2a0e4ef0b00967c6
Louisa Ongenyi <o.louisa@ymail.com>
2023-01-20 14:07:18 +0100

    Tentative authentification 1

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,4 +1,25 @@
+// let user = {
+//         email: "sophie.bluel@test.tld",
+//         password: "S0phie"
+//     };
+//     fetch("http://localhost:5678/api/users/login", {
+//         method: "post",
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(user)
+    
+//     })
+//         .then(response => response.json())
+//         .then(response1 => {
+//             console.log(response1)
+//         });
+
+
+
 // Fonction pour récupérer les données du user dans l'API 
+
 
 async function getUserData(_email, _password) {
     let user = {
@@ -30,23 +51,29 @@ button.addEventListener("click", function (e) {
     e.preventDefault();
     let usermail = mail.value;
     let userpass = pass.value;
-    let data = getUserData(mail.value, pass.value);
+    let data = getUserData(usermail, userpass);
+    // localStorage.setItem()
+  
 
-    if (usermail === "sophie.bluel@test.tld" && userpass === "S0phie") {
-        document.location.href = "./homepage_edit.html";
+
+    if (data != null) {
+        // document.location.href = "./homepage_edit.html";
         console.log("C'est ok")
+        console.log(data);
     } else {
         divMessage.innerHTML = "Erreur dans l’identifiant ou le mot de passe.";
         divMessage.style.color = "red";
         divMessage.style.fontSize = "12px";
         divMessage.style.paddingTop = "60px";
-        console.log("Pas ok")
+        console.log("Pas ok");
+        console.log(data);
+
     }
 
 })
 
 
-async function envoyerFormulaire() {
-    let data = await getUserData;
-    ajoutEventListener(data);
-}
+// async function envoyerFormulaire() {
+//     let data = await getUserData;
+//     ajoutEventListener(data);
+// }
