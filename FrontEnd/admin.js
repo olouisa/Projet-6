cb9b1897d83387f7e5966befca038820454bc73f
Louisa Ongenyi <o.louisa@ymail.com>
2023-01-27 10:15:42 +0100

    Tentative ajout bouton poubelle

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -25,3 +25,117 @@ btnCloseModal.addEventListener("click", closeModal);
 
 const overlay = document.querySelector("#overlay");
 overlay.addEventListener("click", closeModal);
+
+// Récupération des catégories
+fetch("http://localhost:5678/api/categories", {
+    headers: {
+        'Accept': 'application/json',
+    }
+})
+    .then(response => response.json()
+    )
+    .then(response1 => {
+        console.log(response1)
+    });
+
+
+
+// Récupération et affichage des works de la gallerie
+
+// Fonction pour récupérer les données works de l'API
+async function getAllWorks() {
+    const response = await fetch("http://localhost:5678/api/works", {
+        headers: {
+
+            'Accept': 'application/json'
+
+
+        }
+    })
+    return await response.json()
+}
+
+
+// Fonction pour afficher les images de la gallerie
+function AffichageImages(response) {
+    let tab = response.length;
+    console.log(tab);
+    let gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+
+    for (let i = 0; i < tab; i++) {
+
+
+        const img = document.createElement("img");
+        img.setAttribute("src", response[i].imageUrl);
+        img.setAttribute("crossorigin", "anonymous");
+
+
+        const titre = document.createElement("p");
+        titre.innerHTML = response[i].title;
+
+
+        const figure = document.createElement("div");
+        gallery.appendChild(figure);
+        figure.appendChild(img);
+        figure.appendChild(titre);
+
+
+    }
+}
+
+// Fonction pour afficher tous les works 
+async function AfficherTousWorks() {
+    let data = await getAllWorks()
+    console.log(data);
+    AffichageImages(data)
+}
+AfficherTousWorks()
+
+
+// Filtre des objets
+const btnobjets = document.querySelector("#objets");
+btnobjets.addEventListener("click", async function () {
+    let data = await getAllWorks()
+    const filtreObjets = data.filter(function (objet) {
+        return objet.categoryId == 1
+    })
+    console.log(data);
+    AffichageImages(filtreObjets)
+});
+
+
+
+
+// Filtre des appartements
+const btnappartements = document.querySelector("#appartements");
+btnappartements.addEventListener("click", async function () {
+    let data = await getAllWorks();
+    filtreAppartements = data.filter(function (appartement) {
+        return appartement.categoryId == 2
+    })
+    console.log(filtreAppartements);
+    AffichageImages(filtreAppartements);
+});
+
+
+// Filtre des hôtels
+const btnhotels = document.querySelector("#hotels");
+btnhotels.addEventListener("click", async function () {
+    let data = await getAllWorks();
+    filtreHotels = data.filter(function (hotel) {
+        return hotel.categoryId == 3
+    })
+    console.log(filtreHotels);
+    AffichageImages(filtreHotels);
+
+});
+
+
+// Bouton "Tous"
+const btntous = document.querySelector("#tous");
+btntous.addEventListener("click", function () {
+    AfficherTousWorks();
+});
+
+
---
+++ FrontEnd/modale.js
@@ -0,0 +1,138 @@
+
+// Fonction pour supprimer les works
+async function deleteWorks() {
+    let id = response[i].id;
+    const response = await fetch('http://localhost:5678/api/works/${id}', {
+        method: "DELETE",
+        body: null,
+        headers: {
+            'Accept': '*/*',
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY3MzI1ODIzNiwiZXhwIjoxNjczMzQ0NjM2fQ.E-2ipCmskbEKBS74kK-Gx0u1sIBMFT8J0yegaN8L2dw'
+
+        }
+
+    })
+    return await response.json()
+
+}
+
+
+// / Récupération et affichage des works de la gallerie
+
+// Fonction pour récupérer les données works de l'API
+async function getAllWorks() {
+    const response = await fetch("http://localhost:5678/api/works", {
+        headers: {
+
+            'Accept': 'application/json'
+
+
+        }
+    })
+    return await response.json()
+
+}
+
+
+// Fonction pour afficher les images de la gallerie
+function AffichageImages(response) {
+    let tab = response.length;
+    console.log(tab);
+    let gallery = document.querySelector("#modal_gallery");
+    gallery.innerHTML = "";
+    gallery.style.display = "flex";
+    gallery.style.flexDirection = "row";
+    gallery.style.justifyContent = "space-between";
+    gallery.style.flexWrap = "wrap";
+
+
+    for (let i = 0; i < tab; i++) {
+
+
+        const img = document.createElement("img");
+        img.setAttribute("src", response[i].imageUrl);
+        img.setAttribute("crossorigin", "anonymous");
+        img.style.width = "78px";
+        img.style.height = "104px";
+        img.innerHTML = '<button><i class="fa-solid fa-trash-can"></i></button>';
+
+        console.log(img);
+
+        // const btnDelete = document.createElement("button");
+        // btnDelete.style.backgroundColor = "black";
+        // btnDelete.style.width = "17px";
+        // btnDelete.style.height = "17px";
+        // btnDelete.style.position = "absolute";
+        // btnDelete.style.top = "10px";
+        // btnDelete.style.right = "6px";
+        // console.log(btnDelete)
+
+
+
+
+        console.log(response[i].id);
+        // console.log(response[i].imageUrl);
+        let images = response[i].imageUrl;
+        console.log(images);
+
+
+
+
+
+        const titre = document.createElement("p");
+        titre.innerHTML = response[i].title;
+        titre.style.fontSize = "12px";
+        titre.style.width = "35px";
+
+
+        const figure = document.createElement("div");
+        // figure.style.position = "relative";
+        gallery.appendChild(figure);
+        figure.appendChild(img);
+        figure.appendChild(titre);
+        // figure.appendChild(btnDelete);
+        // img.appendChild(btnDelete);
+
+
+
+
+
+    }
+}
+
+// Fonction pour afficher tous les works
+async function AfficherTousWorks() {
+    let data = await getAllWorks()
+    console.log(data);
+    AffichageImages(data)
+}
+AfficherTousWorks();
+
+
+// Fonction pour supprimer
+
+async function deleteOneWork() {
+    let data = await getAllWorks()
+    console.log(data);
+    let tab = data.length;
+    console.log(tab);
+
+
+    // data.forEach(element => {
+
+    //     const btnDelete = document.createElement("button");
+    //     btnDelete.style.backgroundColor = "black";
+    //     btnDelete.style.width = "17px";
+    //     btnDelete.style.height = "17px";
+    //     btnDelete.style.position = "absolute";
+    //     btnDelete.style.top = "10px";
+    //     btnDelete.style.right = "6px";
+    //     console.log(btnDelete)
+
+    // });
+
+}
+deleteOneWork();
+
+
+
